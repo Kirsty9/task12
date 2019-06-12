2ced1e89c2f317032bea54cfe38775679c6457d1
admin <admin@DESKTOP-0CALJDH>
2019-06-12 16:45:40 +0800

    12

---
+++ KQY201731950108_MVC/Models/MyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task12.Models
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        public ActionResult Index()
+        {
+            ViewBag.aaa = "这是我的控制器的index的动作内容kirsty";
+            return View();
+        }
+    }
+}
